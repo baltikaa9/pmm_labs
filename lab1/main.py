acd686b2a3f0dd37529f9a8455f443edc633f1ac
baltikaa9 <sergey4016@mail.ru>
2023-09-14 23:09:59 +0300

    parameters

--- lab1/main.py
+++ lab1/main.py
@@ -6,44 +6,43 @@ from typing import Callable
 
 Coords = Temperature = list[float]
 
-a = 0
-b = 1
-h = 0.02
-N = int((b - a) / h)
-gu_a = 0
-gu_b = 0
 
-x = [0.0 for _ in range(N + 2)]
-for i in range(N + 2):
-    x[i] = round(a + (i - 0.5) * h, 2)
+class Parameters:
+    def __init__(self, a: int, b: int, h: float, t_max: float, t_a: float, t_b: float):
+        self.a = a
+        self.b = b
+        self.h = h
+        self.t_max = t_max
+        self.T_a = t_a
+        self.T_b = t_b
 
-T0 = [round(1 - _x, 2) for _x in x]
+        self.N = int((self.b - self.a) / self.h)
+        self.x = [0.0 for _ in range(self.N + 2)]
+        for i in range(self.N + 2):
+            self.x[i] = round(self.a + (i - 0.5) * self.h, 2)
+        self.T0 = [round(1 - x, 2) for x in self.x]
+        self.tau = self.h * self.h / (2 * self.D(-1))
 
-
-def D(x: float) -> int:
-    return 1 if x < 0.5 else 2
-
-
-t = 0
-tau = h * h / (2 * D(x[-1]))
-t_max = 4
+    def D(self, i: int) -> int:
+        return 1 if self.x[i] < 0.5 else 2
 
 
-def thermal_conductivity(x: Coords, T0: Temperature, d: Callable[[float], int] = (lambda _: 1)):
+def thermal_conductivity(params: Parameters):
     t = 0
-    T1 = [0.0 for _ in range(N + 2)]
-    while t < t_max:
-        for i in range(1, N + 1):
-            k = (tau * d(x[i])) / (h * h)
-            T1[i] = k * T0[i + 1] + (1 - 2 * k) * T0[i] + k * T0[i - 1]
+    T1 = [0.0 for _ in range(params.N + 2)]
+    while t < params.t_max:
+        for i in range(1, params.N + 1):
+            k = (params.tau * params.D(i)) / (params.h * params.h)
+            T1[i] = k * params.T0[i + 1] + (1 - 2 * k) * params.T0[i] + k * params.T0[i - 1]
 
-        T1[0] = gu_a - T1[1]
-        T1[N + 1] = gu_b - T1[N]
+        T1[0] = params.T_a - T1[1]
+        T1[params.N + 1] = params.T_b - T1[params.N]
         print(T1[1:-1])
-        T0 = T1.copy()
-        t += tau
+        params.T0 = T1.copy()
+        t += params.tau
 
 
 if __name__ == '__main__':
-    thermal_conductivity(x, T0, D)
+    params = Parameters(0, 1, 0.02, 4, 0, 0)
+    thermal_conductivity(params)
     ...
