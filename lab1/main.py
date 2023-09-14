4cd69a6992e7f8db9df37c79b3729b16c36f3dab
baltikaa9 <sergey4016@mail.ru>
2023-09-14 23:20:41 +0300

    parameters

--- lab1/main.py
+++ lab1/main.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import Callable
 
 """
@@ -7,20 +8,23 @@ from typing import Callable
 Coords = Temperature = list[float]
 
 
+@dataclass
 class Parameters:
-    def __init__(self, a: int, b: int, h: float, t_max: float, t_a: float, t_b: float):
-        self.a = a
-        self.b = b
-        self.h = h
-        self.t_max = t_max
-        self.T_a = t_a
-        self.T_b = t_b
+    a: int
+    b: int
+    h: float
+    t_max: int
+    T_a: float
+    T_b: float
 
+    def __post_init__(self):
         self.N = int((self.b - self.a) / self.h)
         self.x = [0.0 for _ in range(self.N + 2)]
         for i in range(self.N + 2):
             self.x[i] = round(self.a + (i - 0.5) * self.h, 2)
         self.T0 = [round(1 - x, 2) for x in self.x]
+        self.T0[0] = self.T_a
+        self.T0[-1] = self.T_b
         self.tau = self.h * self.h / (2 * self.D(-1))
 
     def D(self, i: int) -> int:
@@ -43,6 +47,6 @@ def thermal_conductivity(params: Parameters):
 
 
 if __name__ == '__main__':
-    params = Parameters(0, 1, 0.02, 4, 0, 0)
+    params = Parameters(a=0, b=1, h=0.02, t_max=4, T_a=0, T_b=0)
     thermal_conductivity(params)
     ...
